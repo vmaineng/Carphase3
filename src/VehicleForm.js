04f71e03de240580a4d4d4b2066d9dab2152a60c
Mai Vang <vmaineng@gmail.com>
2022-06-23 09:55:24 -0500

    updated the create new reservation

--- src/VehicleForm.js
+++ src/VehicleForm.js
@@ -26,6 +26,22 @@ function handleSubmit(e) {
       setReservation(parkedcars)};
     })
   };
+
+  function handleNewReservation(e) {
+    e.preventDefault();
+  
+      fetch("http://localhost:9292/parkedcars", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          vehicle: vehicle
+        }),
+      })
+      .then(r => r.json())
+      .then((parkedcars) => handleAddReservation(parkedcars))
+      }
  
 //add a new vehicle
 function handleAddReservation(newReservation) {
@@ -79,9 +95,17 @@ function handleAddReservation(newReservation) {
       </form>
       
       {errors && <p>{errors}</p>}
-
+      <form onSubmit={handleNewReservation}>
+        <input 
+        type="text"
+        name="name"
+        placeholder="name"
+        value={ vehicle}
+        onChange={(e) => setVehicle(e.target.value)}
+        />
       <button type="submit">Make new reservation</button>
-      
+      </form>
+
       <VehicleParked 
           reservations={reservations} 
           onRemoveItem={handleRemoveItem}
